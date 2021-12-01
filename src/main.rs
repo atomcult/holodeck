cc4b728c1d2e716ff62f6259339af3ef5120c4d1
jbrock <joseph.brock@protonmail.com>
2021-12-01 16:59:49 -0500

    Make sure child processes are reaped

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::fs::File;
 use std::collections::HashMap;
+use std::process::Command;
 
 use evdev::{Device, InputEventKind};
 use serde_derive::Deserialize;
@@ -33,24 +34,57 @@ fn config() -> Result<Config> {
    Ok(toml::from_str::<Config>(&s)?)
 }
 
-fn main() -> Result<()> {
-    let cfg = config()?;
+fn print_supported_keys(kbd: &Device) -> Result<()> {
+    if let Some(supported_keys) = kbd.supported_keys() {
+        for key in supported_keys.iter() {
+            println!("{:#?}", key);
+        }
+    }
 
-    let mut deck = Device::open(cfg.device.unwrap())?;
-    deck.grab()?;
+    Ok(())
+}
 
-    loop {
-        let event_batch = deck.fetch_events()?;
-        for event in event_batch {
-            if let InputEventKind::Key(key) = event.kind() {
-                if event.value() == keystate::PRESSED {
-                    if let Some(cmd) = cfg.binds.get(&format!("{:?}", key)) {
-                        std::process::Command::new(&cmd[0])
-                            .args(&cmd[1..])
-                            .spawn()?;
-                    }
+fn identify_keys(kbd: &mut Device) -> Result<()> {
+    let event_batch = kbd.fetch_events()?;
+    for event in event_batch {
+        if let InputEventKind::Key(key) = event.kind() {
+            if event.value() == keystate::PRESSED {
+                println!("{:?}", key);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn parse_hotkeys(kbd: &mut Device, binds: &HashMap<String, Vec<String>>) -> Result<()> {
+    let event_batch = kbd.fetch_events()?;
+    for event in event_batch {
+        if let InputEventKind::Key(key) = event.kind() {
+            if event.value() == keystate::PRESSED {
+                if let Some(cmd) = binds.get(&format!("{:?}", key)) {
+                    let mut child = Command::new(&cmd[0])
+                        .args(&cmd[1..])
+                        .spawn()?;
+
+                    std::thread::spawn(move || {
+                        child.wait().unwrap();
+                    });
                 }
             }
         }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let cfg = config()?;
+
+    let mut kbd = Device::open(cfg.device.unwrap())?;
+    kbd.grab()?;
+
+    loop {
+        parse_hotkeys(&mut kbd, &cfg.binds)?;
     }
 }
